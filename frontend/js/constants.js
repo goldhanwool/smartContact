7450901bf52bb5408bdb0ff4c07541dd4b5d5c84
goldhanwool <70783496+goldhanwool@users.noreply.github.com>
2022-06-03 23:49:14 +0900

    'nfft-contract'

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x70F9e453FC6CeDf2A3505D8bf4d2433123232D9B"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
